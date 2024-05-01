f93be18cdf42d6765da8ee4794c2b71a3ed0162e
llckumadevelop <kumauser01@kuma.ink>
2024-05-01 16:09:34 +0700

    up code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import RenderGlasses from "./components/RenderGlasses";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container-fluid p-0 vh-100" style={{ backgroundImage: "url(./assets/imgs/glassesImage/background.jpg)", backgroundSize: "cover" }}>
+      <div className="bg-page p-0 vh-100" style={{ backgroundColor: "rgba(0,0,0,0.7)" }}>
+        <h1 className="py-3 px-2 text-white text-center" style={{ backgroundColor: "rgba(0,0,0,0.8)" }}>
+          glasses project
+        </h1>
+        <RenderGlasses />
+      </div>
     </div>
   );
 }
